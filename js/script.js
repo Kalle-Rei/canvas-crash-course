c7accc93f0de94a0a3f4d51b3017d1462e7a160f
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-02-15 11:39:40 +0100

    added animation #1

--- js/script.js
+++ js/script.js
@@ -51,32 +51,76 @@ const ctx = canvas.getContext("2d");
 // ctx.fillStyle = "teal";
 // ctx.fill();
 
-// Arc (circles)
-ctx.beginPath();
+// // Arc (circles)
+// ctx.beginPath();
 
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
 
-// Draw head
-ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+// // Draw head
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
 
-// Move to mouth
-ctx.moveTo(centerX + 100, centerY);
+// // Move to mouth
+// ctx.moveTo(centerX + 100, centerY);
 
-// Draw mouth
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+// // Draw mouth
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
 
-// Move to left eye
-ctx.moveTo(centerX - 60, centerY - 80);
+// // Move to left eye
+// ctx.moveTo(centerX - 60, centerY - 80);
 
-// Draw left eye
-ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+// // Draw left eye
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
 
-// Move to right eye
-ctx.moveTo(centerX + 100, centerY - 80);
+// // Move to right eye
+// ctx.moveTo(centerX + 100, centerY - 80);
 
-// Draw right eye
-ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
+// // Draw right eye
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
 
 
-ctx.stroke();
+// ctx.stroke();
+
+// Animation 1
+
+const circle = {
+    x: 200,
+    y: 200,
+    size: 30,
+    dx: 5,
+    dy: 4
+}
+
+function drawCircle() {
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+    ctx.fillStyle = "purple";
+    ctx.fill();
+}
+
+function update(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCircle();
+
+    // change position
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+
+    // Detect side walls
+    if(circle.x + circle.size > canvas.width  || circle.x - 
+        circle.size < 0) {
+        circle.dx *= -1;
+    }
+
+    // Detect top and bottom walls
+    if(circle.y + circle.size > canvas.height || circle.y - 
+        circle.size < 0) {
+            circle.dy *= -1;
+    }
+
+
+    requestAnimationFrame(update);
+
+}
+
+update();
